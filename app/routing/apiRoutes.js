aee9dad85796ed88ad77a7f76c1e60f7a21faaec
Vram Kupelian <30922280+vramkupelian@users.noreply.github.com>
2018-03-02 21:12:10 -0800

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,28 +16,7 @@ module.exports = function (app){
 
 		var userResponses = userData.scores;
 
-		// Compute best friend match
-		var matchName = '';
-		var matchImage = '';
-		var totalDifference = 10000; // Make the initial value big for comparison
-
-		// Examine all existing friends in the list
-		for (var i = 0; i < friends.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
-
-			// Compute differences for each question
-			var diff = 0;
-			for (var j = 0; j < userResponses.length; j++) {
-				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
-            }
-            
-			// If lowest difference, record the friend match
-			if (diff < totalDifference) {
-				totalDifference = diff;
-				matchName = friends[i].name;
-				matchImage = friends[i].photo;
-			}
-		}
+	
 
 		// Add new user
 		friends.push(userData);
