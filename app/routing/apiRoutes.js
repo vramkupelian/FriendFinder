ee8d508ef1608e7accb3bc5afd094d55cc608922
unknown <vramkupelian@gmail.com>
2018-03-04 18:03:56 -0800

    Scores are stored as integers, not strings. Logic for calculating most compatible complete. Need to do modal.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,9 +15,31 @@ module.exports = function (app){
 		var userData = req.body;
 
 		var userResponses = userData.scores;
+		for(var i =0 ; i< userResponses.length; i++){
+			userResponses[i] = parseInt(userResponses[i]);
+		}
 
-	
+		var matchName = "";
+		var matchImage = "";
+		var differences = [];
 
+		for(var i=0; i<friends.length;i++){
+			
+			var currentDiff= 0 ;
+			
+			for (var j=0; j<userResponses.length;j++){
+				currentDiff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			differences.push(currentDiff);
+		}
+
+		//Index of Match
+		var indexOfMinDiff = differences.indexOf(Math.min(...differences));
+		console.log(indexOfMinDiff);
+
+		matchName = friends[indexOfMinDiff].name;
+		matchImage = friends[indexOfMinDiff].photo;
+		console.log(matchName);
 		// Add new user
 		friends.push(userData);
 
